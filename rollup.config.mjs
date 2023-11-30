70e313b0a930488540af54ad6d426af4bec96110
fum-is-chum <alvinardiansyah2002@gmail.com>
2023-12-01 01:31:11 +0700

    feat: Add new files and update exports

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,9 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import packageJson from "./package.json" assert { type: "json" };
+import postcss from "rollup-plugin-postcss";
+import terser from "@rollup/plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default [
   {
@@ -20,14 +23,18 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
+      terser(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
--- src/components/MiniWallet/MiniWallet.tsx
+++ src/components/MiniWallet/MiniWallet.tsx
@@ -1,5 +1,5 @@
 import type { FC } from 'react';
-
+import './MiniWallet.css';
 type MiniWalletProps = {};
 
 const MiniWallet: FC<MiniWalletProps> = () => {
