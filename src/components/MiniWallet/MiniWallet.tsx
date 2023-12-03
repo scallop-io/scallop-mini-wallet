067e0d255d7f930cbca0e86f59a37ab6c2fcab37
nicetomeetyou1 <han@scallop.io>
2023-12-03 18:12:27 +0700

    feat: add initial state miniwallet

--- src/components/MiniWallet/MiniWallet.tsx
+++ src/components/MiniWallet/MiniWallet.tsx
@@ -1,10 +1,14 @@
 import './miniwallet.scss';
 import React from 'react';
+import { Portfolio } from '@/components/Portfolio';
+import { Zklogin } from '@/components/Zklogin';
 import type { FC } from 'react';
 type MiniWalletProps = {};
 
 const MiniWallet: FC<MiniWalletProps> = () => {
-  return <div className="miniwallet-container" />;
+  const [isConnected, setIsConnected] = React.useState(false);
+
+  return <div className="miniwallet-container">{isConnected ? <Portfolio /> : <Zklogin />}</div>;
 };
 
 export default MiniWallet;
