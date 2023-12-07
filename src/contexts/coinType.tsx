32b674a172dd7db86feca691b011f8fd6b54e5f9
fum-is-chum <alvinardiansyah2002@gmail.com>
2023-12-08 05:25:27 +0700

    fix: minor

--- src/contexts/coinType.tsx
+++ src/contexts/coinType.tsx
@@ -13,7 +13,6 @@ import {
   type CustomCoinType,
   networks,
 } from '@/stores';
-import { DEFAULT_COINS } from '@/constants/coins';
 import { useNetwork } from './connection';
 import { useCoinTypeDB } from './db';
 
@@ -85,10 +84,6 @@ export const LocalCoinTypeProvider: FC<PropsWithChildren<LocalCoinTypeProviderPr
   );
 
   useEffect(() => {
-    // for hackathon purpose, manually merge
-    if (initialCoinTypeState) {
-      initialCoinTypeState.coinTypes = { ...initialCoinTypeState.coinTypes, ...DEFAULT_COINS };
-    }
     for (const network of networks) {
       for (const coinType of initialCoinTypeState?.coinTypes[network] || []) {
         if (coinType.iconUrl) {
